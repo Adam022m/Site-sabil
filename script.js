78fc61fd30d8d35b6f6867c5be4f2074bb8cc09e
Adam_022m <lavitre256@gmail.com>
2024-08-03 01:30:23 +0200

    Navbar fini, manque juste de faire apparaître la croix à la place du menu hamburger et mettre le fond totalement blanc et non transparent

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 const BurgerMenuButton = document.querySelector(".hamburger");
 const BurgerMenu = document.querySelector(".container-links");
 
-BurgerMenuButton.addEventListener('click', () => {
-  BurgerMenuButton.classList.toggle('open');
-  BurgerMenu.classList.toggle('open');
+BurgerMenuButton.addEventListener("click", () => {
+  BurgerMenuButton.classList.toggle("open");
+  BurgerMenu.classList.toggle("open");
 });
-
-// const BurgerMenuButton = document.querySelector(".hamburger");
-// const BurgerMenu = document.querySelector(".container-links");
-
-// BurgerMenuButton.addEventListener('click', () => {
-//   BurgerMenuButton.classList.toggle('open');
-//   BurgerMenu.classList.toggle('open');
-// });
