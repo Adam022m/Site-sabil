a05e4295888a9ca446b09cfa44c21516af6089f8
Adam_022m <lavitre256@gmail.com>
2024-08-03 01:57:36 +0200

    NavBar presque fini, manque de faire en sorte que le logo ne se rapproche pas de la gauche quand la croix est présente, rajouter les flèches qui vont avec les liens de la sidebar et mettre le fond totalement blanc sans transparence

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const BurgerMenuButton = document.querySelector(".hamburger");
 const BurgerMenu = document.querySelector(".container-links");
+const CloseButton = document.querySelector(".close-button");
 
 BurgerMenuButton.addEventListener("click", () => {
   BurgerMenuButton.classList.toggle("open");
   BurgerMenu.classList.toggle("open");
+  CloseButton.classList.toggle("open");
+});
+CloseButton.addEventListener("click", () => {
+  BurgerMenuButton.classList.toggle("open");
+  BurgerMenu.classList.toggle("open");
+  CloseButton.classList.toggle("open");
 });
