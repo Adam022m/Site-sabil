105be59293b2183ca0933a24da0d7e1a8ee82839
Adam_022m <lavitre256@gmail.com>
2024-09-18 18:58:27 +0200

    Code en Js qui permet d'activer la transition ease-out au lieu de l'avoir en CSS et d'empêcher que celle-ci ne s'active tant que l'écran n'est pas plus bas ou égal à 900px

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ CloseButton.addEventListener("click", () => {
   BurgerMenu.classList.toggle("open");
   CloseButton.classList.toggle("open");
 });
+
+new ResizeObserver((entries) => {
+  if (entries[0].contentRect.width <= 990) {
+    BurgerMenu.style.transition = "transform 0.3 ease-out";
+  } else {
+    BurgerMenu.style.transition = "none";
+  }
+}).observe(document.body);
+
+// Permet d'activer la transition ease out au lieu de l'avoir en css et de l'empêcher quand la taille de l'écran n'est pas en plus bas ou égal à 900px
