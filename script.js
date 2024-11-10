b001358dc84364f33329c1c81e806ab04a68ad5b
Adam_022m <lavitre256@gmail.com>
2024-11-10 23:32:22 +0100

    Amélioration de la navbar, quand celle-ci est ouverte, on peut cliquer que sur ses éléments et non pas sur le reste, le fond est blanc et opaque, on ne voit pas ce qu'il y a derrière comme c'était le cas avant. J'ai fait en ajoutant une balise open à la classe .main-navlinks par Js au click sur le menu hamburger, ensuite au click sur la croix de fermeture la classe s'enlève. J'ai aussi mis un fond plus blanc pour la navbar. Recadrage de la position du titre que j'ai un peu relevé. Le texte de la section about a été replacé de manière à ce qu'il rentre dans le cadre de sa section ce qui n'était pas le cas avant en mode responsive. Manque de faire en sorte que le logo et le bouton don ne se décale pas quand j'ouvre la navbar. Changement de la taille de la police des éléments de la navbar dans la taille normale du site.

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const BurgerMenuButton = document.querySelector(".hamburger");
+const navbarMenu = document.querySelector(".main-navlinks");
 const BurgerMenu = document.querySelector(".container-links");
 const CloseButton = document.querySelector(".close-button");
+const BodyElement = document.querySelector("body");
 
 BurgerMenuButton.addEventListener("click", () => {
   BurgerMenuButton.classList.toggle("open");
   BurgerMenu.classList.toggle("open");
   CloseButton.classList.toggle("open");
+  BodyElement.classList.toggle("navbar-open");
+  navbarMenu.classList.toggle("open");
 });
+
 CloseButton.addEventListener("click", () => {
   BurgerMenuButton.classList.toggle("open");
   BurgerMenu.classList.toggle("open");
   CloseButton.classList.toggle("open");
+  BodyElement.classList.toggle("navbar-open");
+  navbarMenu.classList.toggle("open");
+});
+
+BurgerMenu.addEventListener("click", () => {
+  BurgerMenuButton.classList.toggle("open");
+  BurgerMenu.classList.toggle("open");
+  CloseButton.classList.toggle("open");
+  BodyElement.classList.toggle("navbar-open");
+  navbarMenu.classList.toggle("open");
 });
 
 new ResizeObserver((entries) => {
@@ -20,5 +35,4 @@ new ResizeObserver((entries) => {
     BurgerMenu.style.transition = "none";
   }
 }).observe(document.body);
-
 // Permet d'activer la transition ease out au lieu de l'avoir en css et de l'empêcher quand la taille de l'écran n'est pas en plus bas ou égal à 900px
